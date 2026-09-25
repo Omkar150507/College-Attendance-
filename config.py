5682f57237ee1e47a2ecd3a7c307079c746ab364
Omkar150507 <shiv157303@gmail.com>
2026-09-25 21:10:54 +0530

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# College Attendance System - main branding settings
-COLLEGE_NAME = "Y.N.P. College of Pharmacy"
-COLLEGE_TAGLINE = "Attendance Management System"
-LOGO_FILE = "image/college_logo.png"
