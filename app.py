42c4621e1e5e450d758cf66501b51a35783edf8f
Omkar150507 <shiv157303@gmail.com>
2026-09-25 23:37:15 +0530

    Refactor app.run for improved readability

--- app.py
+++ app.py
@@ -2831,6 +2831,8 @@ def service_worker():
 init_db()
 
 if __name__ == "__main__":
-    app.run(debug=os.environ.get("FLASK_DEBUG", "0") == "1",
-            host=os.environ.get("HOST", "0.0.0.0"),
-            port=int(os.environ.get("PORT", "5000")))
+    app.run(
+        debug=os.environ.get("FLASK_DEBUG", "0") == "1",
+        host=os.environ.get("HOST", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000"))
+    )
